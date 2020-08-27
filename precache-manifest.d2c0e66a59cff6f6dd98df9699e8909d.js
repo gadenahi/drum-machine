9f80ac471d469ee18f1947d3b6799c5e17927224
gadenahi@gmail.com <gadenahi@gmail.com>
2020-08-27 15:49:22 -0700

    Updates

---
+++ precache-manifest.d2c0e66a59cff6f6dd98df9699e8909d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aca334808e5d01d982f69c1fbcfc57e",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "8dddc57352960327163b",
+    "url": "/drum-machine/static/css/main.0cf53bb8.chunk.css"
+  },
+  {
+    "revision": "6e1091d4bde52b9dee7b",
+    "url": "/drum-machine/static/js/2.eb2cb1bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.eb2cb1bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dddc57352960327163b",
+    "url": "/drum-machine/static/js/main.60ae96d7.chunk.js"
+  },
+  {
+    "revision": "1bb2f18031a5f4e4abe0",
+    "url": "/drum-machine/static/js/runtime-main.d68e1a75.js"
+  }
+]);
